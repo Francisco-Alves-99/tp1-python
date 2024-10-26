56b9171ca57c728fe17b50960de3751bd6da395b
Francisco Alves <franciscoalves@MacBook-Air-de-Francisco.local>
2024-10-26 14:36:28 -0300

    ex-1

---
+++ ex-1.py
@@ -0,0 +1,8 @@
+n1 = input("Digite um número: ")
+n2 = input("Digite outro número: ")
+
+print("Soma: ", int(n1) + int(n2))
+print("Subtração: ", int(n1) - int(n2))
+print("Multiplicação: ", int(n1) * int(n2))
+print("Divisão: ", int(n1) / int(n2))
+print("Divisão inteira: ", int(n1) // int(n2))
