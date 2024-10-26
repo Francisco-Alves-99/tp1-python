a2f0eea24e6a607b68cbd069063dac66fa4922d8
Francisco Alves <franciscoalves@MacBook-Air-de-Francisco.local>
2024-10-26 16:01:52 -0300

    ex-15

---
+++ ex-15.py
@@ -0,0 +1,8 @@
+escolha = int(input("Você está em uma masmorra e se depara com 3 portões, qual deles você irá entrar? Digite: 1, 2 ou 3 "))
+
+if escolha == 1:
+    print("Você adentra em uma sala escura onde há um Troll gigante para derrotar")
+elif escolha == 2:
+    print("Você entra em uma floresta que te leva em direção de um castelo") 
+elif escolha == 3:
+    print("Você cai em um buraco com diversas serpentes")       
